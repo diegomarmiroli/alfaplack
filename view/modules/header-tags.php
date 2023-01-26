03f782698e1842cc3c33646f71bd7e93cfb0b114
unknown <diegomarmiroli20@gmail.com>
2023-01-25 21:30:54 -0300

    Agregado de tags header como módulo

---
+++ view/modules/header-tags.php
@@ -0,0 +1,15 @@
+<!-- Importaciones globales del header -->
+
+<title>Alfa Plack</title>
+<!-- Favicon -->
+<link href="img/logos/logo_cuadrado.png" rel="icon">
+
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/style.css">
